78a999fd3bd12148c15715f80968fe3ae9c627dc
bernabarros <bernardobarros.work@gmail.com>
2025-03-25 19:39:24 +0000

    Add RandDice code

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,15 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Random dice_seed = new Random(int.Parse(args[1]));
+            int sum_result = 0;
+
+            for(int n = 1; n <= int.Parse(args[0]); n++)
+            {
+                sum_result += dice_seed.Next(1,7);
+            }
+
+            Console.WriteLine($"{sum_result}");
         }
     }
 }
