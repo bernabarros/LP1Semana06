c2d4de96841aafb3524b2b86ea59a887e213c70a
bernabarros <bernardobarros.work@gmail.com>
2025-03-25 18:33:01 +0000

    Create project Chronos and add code

---
+++ Chronos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            string elapsedtime1;
+            string elapsedtime2;
+
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+            crono1.Stop();
+            crono2.Stop();
+
+            //TimeSpan ts1 = crono1.Elapsed;
+            //TimeSpan ts2 = crono2.Elapsed;
+
+            //elapsedtime1 = String.Format("{0:f4}", ts1.Milliseconds);
+            //elapsedtime2 = String.Format("{0:f4}", ts2.Milliseconds);
+
+            Console.WriteLine($"{crono1.Elapsed.TotalMilliseconds / 1000:f4}");
+            Console.WriteLine($"{crono2.Elapsed.TotalMilliseconds / 1000:f4}");
+        }
+    }
+}
