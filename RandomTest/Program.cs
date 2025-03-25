f50a84ec1b8745d7b39016adcd40b66e0ca6e7a3
bernabarros <bernardobarros.work@gmail.com>
2025-03-25 19:16:31 +0000

    Create project RandDice

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -6,8 +6,8 @@ namespace RandomTest
     {
         private static void Main(string[] args)
         {
-            Random r1 = new Random(22);
-            Random r2 = new Random(22);
+            Random r1 = new Random(23);
+            Random r2 = new Random(23);
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
             Console.WriteLine();
